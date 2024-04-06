b1bbc786f869222982faca6ec3140961811b656d
@hexlet/cli <support@hexlet.io>
2024-04-06 16:22:39 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,32 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
+  def setup
+    @stack_element = Stack.new
+  end
   
+  def test_was_element_added_with_push!
+    expected_result = %w(ruby)
+    assert(@stack_element.push!('ruby') == expected_result)
+  end
+
+  def test_was_element_deleted_with_pop!
+    poped_element = 'ruby'
+    @stack_element.push!('ruby')
+    assert(@stack_element.pop! == poped_element)
+  end
+
+  def test_was_element_cleared_with_clear!
+    expected_result = []
+    @stack_element.push!('ruby')
+    assert(@stack_element.clear! == expected_result)
+  end
+
+  def test_empty_method_from_stack
+    assert(@stack_element.empty?)
+  end
+
+  def teardown; end
   # END
 end
 
