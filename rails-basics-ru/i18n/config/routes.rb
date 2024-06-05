2117eade6eb183c1b2488a953a009c5fab5425dc
@hexlet/cli <support@hexlet.io>
2024-06-05 23:40:33 +0300

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -2,7 +2,16 @@
 
 Rails.application.routes.draw do
   # BEGIN
-  
+  locales = I18n.available_locales.join('|')
+  scope '/(:locale)', locale: /#{locales}/ do
+    root to: 'home#index'
+
+    resources :posts do
+      scope module: :posts do
+        resources :comments, path: 'post_comments'
+      end
+    end
+  end
   # END
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
