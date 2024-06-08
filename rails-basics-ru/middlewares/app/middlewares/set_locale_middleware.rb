d4b9cd83778dd949a7583ba742818df29e941da1
@hexlet/cli <support@hexlet.io>
2024-06-08 22:10:41 +0300

    submit rails-basics-ru/middlewares

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -6,6 +6,26 @@ class SetLocaleMiddleware
   end
 
   # BEGIN
-  
+  def call(env)
+    dup._call(env)
+  end
+
+  def _call(env)
+    Rails.logger.debug "* Accept language: #{env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_accept_language_header(env) || I18n.default_locale
+    I18n.locale = locale
+    Rails.logger.debug "* Locale set to '#{locale}'"
+    
+    @status, @headers, @response = @app.call(env)
+    [@status, @headers, @response]
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header(env)
+    return if env['HTTP_ACCEPT_LANGUAGE'].blank?
+
+    env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
   # END
 end
